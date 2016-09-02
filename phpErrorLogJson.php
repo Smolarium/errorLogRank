54dcaf9ab12c23471d84f449f110565974f8498a
Krzysztof Białoń <krzysztof.bialon@librus.pl>
2016-09-02 12:47:53 +0200

    Wyeliminowałem mruganie z odźwieżania strony.
    Dodałem skrypt `phpErrorLogJson.php` który zwraca te same dane w formacje *json*.
    Dodałem skrypt show.html który parsuje i wyświetla te dane. Pobiera je ajaxem więc skrypt nie mruga.

---
+++ phpErrorLogJson.php
@@ -0,0 +1,57 @@
+<?php
+
+$iniConf = parse_ini_file("errorLoger.ini");
+$redis = new \Redis();
+$redis->connect($iniConf['REDIS_HOST'], $iniConf['REDIS_PORT']);
+if (isset($iniConf['REDIS_PASS'])) {
+    $redis->auth($iniConf['REDIS_PASS']);
+}
+
+if (isset($iniConf['REDIS_DATABASE_INDEX'])) {
+    $redis->select($iniConf['REDIS_DATABASE_INDEX']);
+}
+
+$fatals = $redis->zRevRange($iniConf['REDIS_KEY_PARSED_FATAL'], 0, $iniConf['SHOW_THIS_MANY_FATALS'], true);
+$warnings = $redis->zRevRange($iniConf['REDIS_KEY_PARSED_WARNING'], 0, $iniConf['SHOW_THIS_MANY_WARNINGS'], true);
+$notices = $redis->zRevRange($iniConf['REDIS_KEY_PARSED_NOTICE'], 0, $iniConf['SHOW_THIS_MANY_NOTICES'], true);
+$deprecateds = $redis->zRevRange(
+    $iniConf['REDIS_KEY_PARSED_DEPRECATED'],
+    0,
+    $iniConf['SHOW_THIS_MANY_DEPRECATEDS'],
+    true
+);
+
+$res = ['fatals' => [], 'warnings' => [], 'notices' => [], 'deprecateds' => []];
+foreach ($fatals as $text => $count) {
+    $res['fatals'][] = [
+        'id' => hash('sha256', $text),
+        'content' => $text,
+        'count' => $count
+    ];
+}
+
+foreach ($warnings as $text => $count) {
+    $res['warnings'][] = [
+        'id' => hash('sha256', $text),
+        'content' => $text,
+        'count' => $count
+    ];
+}
+
+foreach ($notices as $text => $count) {
+    $res['notices'][] = [
+        'id' => hash('sha256', $text),
+        'content' => $text,
+        'count' => $count
+    ];
+}
+
+foreach ($deprecateds as $text => $count) {
+    $res['deprecateds'][] = [
+        'id' => hash('sha256', $text),
+        'content' => $text,
+        'count' => $count
+    ];
+}
+
+echo json_encode($res);
